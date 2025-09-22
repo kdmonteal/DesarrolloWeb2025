9ab4fd07c21f0fde5d89aaaed6769020b8c13978
kdmonteal <kdmonteal@gmail.com>
2025-09-22 10:56:50 -0500

    creacion Tracks js

---
+++ reproductorSpotify/src/data/tracks.js
@@ -0,0 +1,11 @@
+// src/data/tracks 1 **
+
+const tracks = [
+    {
+        id: 1,
+        title: "Sugar",
+        artist: "Maroon 5",
+        src: "/tracks/Maroon_5_Sugar.mp3",
+        cover: "./covers/sugar.jpg"
+    }
+];
